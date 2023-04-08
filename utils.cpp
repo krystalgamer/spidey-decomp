2195d84ca675a7233ca2ca6433d06287b21013e3
Jose Silva <callofdutypsp@gmail.com>
2023-04-08 18:09:56 +0200

    Utils_CrapDist done

---
+++ utils.cpp
@@ -0,0 +1,45 @@
+#include "utils.h"
+
+
+int Utils_CrapDist(const CVector& a,const CVector& b){
+
+	
+	int dX = a.vx - b.vx;
+	int dY = a.vy - b.vy;
+	int dZ = a.vz - b.vz;
+
+    if (dX < 0) {
+        dX = -dX;
+    }
+
+    if (dY < 0) {
+        dY = -dY;
+    }
+
+    if (dZ < 0) {
+        dZ = -dZ;
+    }
+
+    if (dX < dY){
+
+        if (dY < dZ){
+			return (dZ + (dX >>2) + (dY >>1)) >> 12;
+		}
+
+		if(dZ < dY){
+			return ((dZ >> 2) + (dX >> 1) + dY) >> 12;
+		}
+
+		return ((dZ >> 1) + (dX >> 2) + dY) >> 12;
+    }
+
+    if (dX < dZ){
+        return (dZ + (dX >>1) + (dY >>2)) >> 12;
+    }
+
+    if (dZ < dY){
+        return ((dZ >> 2) + dX + (dY >>1)) >> 12;
+    }
+
+    return ((dZ >> 1) + dX + (dY >>2)) >> 12;
+}
---
+++ utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "export.h"
+#include "vector.h"
+
+
+EXPORT int Utils_CrapDist(const CVector& a,const CVector& b) ;
