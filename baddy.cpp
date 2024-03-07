9866c88a6baa68fa4974f7e6a70c76cac425dc87
krystalgamer <callofdutypsp@gmail.com>
2024-03-07 20:23:33 +0100

    More CBaddy fields

--- baddy.cpp
+++ baddy.cpp
@@ -48,9 +48,18 @@ int CBaddy::TugWeb(void){
 }
 
 void validate_CBaddy(void){
+	VALIDATE(CBaddy, field_1A8, 0x1A8);
+	VALIDATE(CBaddy, field_1F4, 0x1F4);
 	VALIDATE(CBaddy, field_204, 0x204);
 	VALIDATE(CBaddy, field_208, 0x208);
 	VALIDATE(CBaddy, field_21E, 0x21E);
+	VALIDATE(CBaddy, field_27C, 0x27C);
+	VALIDATE(CBaddy, field_280, 0x280);
+	VALIDATE(CBaddy, field_284, 0x284);
 	VALIDATE(CBaddy, field_290, 0x290);
 	VALIDATE(CBaddy, field_2A8, 0x2A8);
+
+	VALIDATE(CBaddy, field_2F0, 0x2F0);
+	VALIDATE(CBaddy, field_2F4, 0x2F4);
+	VALIDATE(CBaddy, field_2FC, 0x2FC);
 }
--- baddy.h
+++ baddy.h
@@ -11,8 +11,13 @@ public:
 	EXPORT int TrapWeb(void);
 	EXPORT int TugWeb(void);
 
+	unsigned char asdfaPad[0x14];
+	CVector field_1A8;
 
-	unsigned char pad[0x69+4];
+	unsigned char baddyPad[0x40];
+	int field_1F4;
+
+	unsigned char pad[0x9];
 
 	int field_204;
 	int field_208;
@@ -20,11 +25,24 @@ public:
 	unsigned char morerererPad[0x12];
 	__int16 field_21E;
 
-	unsigned char morerPad[0x84 - 0x14];
+	unsigned char morerPad[0x84 - 0x14 - 4 - 0x10];
+	int field_27C;
+	int field_280;
+	int field_284;
+	
+	unsigned char weirdpad[0x06];
 	int field_290;
 	unsigned char morePad[0xA0 - 8 - 0x84];
 
 	int field_2A8;
+
+	unsigned char shitPad[0x50 - 0xC];
+
+	int field_2F0;
+	int field_2F4;
+
+	unsigned char shitterPad[0x4];
+	CVector field_2FC;
 
 };
 
