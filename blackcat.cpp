dd44cd48d7c727cf96472f5dff8490293d68817d
krystalgamer <callofdutypsp@gmail.com>
2024-03-09 13:00:06 +0100

    Started CBlackCat

--- blackcat.cpp
+++ blackcat.cpp
@@ -3,4 +3,14 @@
 
 void validate_CBlackCat(void){
 	VALIDATE_SIZE(CBlackCat, 0x354);
+
+
+	VALIDATE(CBlackCat, field_324, 0x324);
+	VALIDATE(CBlackCat, field_32C, 0x32C);
+
+
+	VALIDATE(CBlackCat, field_340, 0x340);
+	VALIDATE(CBlackCat, field_348, 0x348);
+
+	VALIDATE(CBlackCat, field_34C, 0x34C);
 }
--- blackcat.h
+++ blackcat.h
@@ -4,6 +4,21 @@
 #include "baddy.h"
 
 class CBlackCat : public CBaddy {
+public:
+	
+	int field_324;
+	unsigned char padAfter324[0x4];
+	int field_32C;
+	unsigned char padAfter32C[0x10];
+
+	unsigned char field_340;
+	unsigned char padAfter340[0x7];
+
+	unsigned char field_348;
+
+	int field_34C;
+
+	int finalNotDefinedField;
 };
 
 void validate_CBlackCat(void);
