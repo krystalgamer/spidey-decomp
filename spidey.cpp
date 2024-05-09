2627283456bb0f18f0b0bcd0d5b68823869552c0
krystalgamer <callofdutypsp@gmail.com>
2024-05-09 19:08:39 +0200

    CPlayer::SetIgnoreInputTimer

--- spidey.cpp
+++ spidey.cpp
@@ -1,6 +1,18 @@
 #include "spidey.h"
 #include "validate.h"
 
+void CPlayer::SetIgnoreInputTimer(int a2)
+{
+	this->field_E18 = a2;
+	if (a2)
+	{
+		this->field_E12 = this->csuperend;
+		if (this->field_8EA)
+		{
+			this->ExitLookaroundMode();
+		}
+	}
+}
 
 // @Ok
 void CPlayer::SetCamAngleLock(unsigned __int16 a1)
@@ -15,9 +27,19 @@ void CPlayer::SetCamAngleLock(unsigned __int16 a1)
 	}
 }
 
+
+// @TODO
+void CPlayer::ExitLookaroundMode(void)
+{}
+
 void validate_CPlayer(void)
 {
 	VALIDATE_SIZE(CPlayer, 0xEFC);
 
+	VALIDATE(CPlayer, field_8EA, 0x8EA);
+
 	VALIDATE(CPlayer, gCamAngleLock, 0x8EC);
+
+	VALIDATE(CPlayer, field_E12, 0xE12);
+	VALIDATE(CPlayer, field_E18, 0xE18);
 }
--- spidey.h
+++ spidey.h
@@ -11,14 +11,26 @@ class CPlayer : public CSuper
 {
 	public:
 
-		unsigned char padTopPlayer[0x8EC-0x194];
+		unsigned char padTopPlayer[0x8EA-0x194];
 
-		unsigned char gCamAngleLock;
+		unsigned char field_8EA;
+		unsigned char padAFter8Ea;
 
-		unsigned char padBottomPlayer[0xEFC-0x8EC-1];
+		unsigned char gCamAngleLock; //8EC
+		unsigned char padAfterLock[0xE12-0x8EC-1];
+
+
+		__int16 field_E12;
+		unsigned char padAfterE12[4];
+
+		int field_E18;
+
+		unsigned char padBottomPlayer[0xEFC-0xE18-4];
 
 
 		EXPORT void SetCamAngleLock(unsigned __int16);
+		EXPORT void ExitLookaroundMode(void);
+		EXPORT void SetIgnoreInputTimer(int);
 };
 
 void validate_CPlayer(void);
