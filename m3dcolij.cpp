fed0829199570d7d22281edf43b0217dad304cbe
krystalgamer <callofdutypsp@gmail.com>
2024-05-13 18:26:01 +0200

    Added fields to SLineInfo

--- m3dcolij.cpp
+++ m3dcolij.cpp
@@ -8,8 +8,36 @@ void M3dColij_InitLineInfo(SLineInfo *)
 
 void validate_SLineInfo(void)
 {
-	VALIDATE_SIZE(SLineInfo, 0xC * 2);
+	VALIDATE_SIZE(SLineInfo, 0x90);
 
 	VALIDATE(SLineInfo, vec_0, 0x0);
 	VALIDATE(SLineInfo, vec_C, 0xC);
+
+
+	VALIDATE(SLineInfo, field_18, 0x18);
+	VALIDATE(SLineInfo, field_1C, 0x1C);
+	VALIDATE(SLineInfo, field_20, 0x20);
+	VALIDATE(SLineInfo, field_24, 0x24);
+	VALIDATE(SLineInfo, field_28, 0x28);
+	VALIDATE(SLineInfo, field_2C, 0x2C);
+
+
+	VALIDATE(SLineInfo, field_40, 0x40);
+	VALIDATE(SLineInfo, field_44, 0x44);
+	VALIDATE(SLineInfo, field_48, 0x48);
+	VALIDATE(SLineInfo, field_68, 0x68);
+
+	VALIDATE(SLineInfo, field_6C, 0x6C);
+	VALIDATE(SLineInfo, field_70, 0x70);
+	VALIDATE(SLineInfo, field_74, 0x74);
+	VALIDATE(SLineInfo, field_78, 0x78);
+	VALIDATE(SLineInfo, field_7A, 0x7A);
+	VALIDATE(SLineInfo, field_7C, 0x7C);
+
+	VALIDATE(SLineInfo, field_80, 0x80);
+	VALIDATE(SLineInfo, field_84, 0x84);
+	VALIDATE(SLineInfo, field_88, 0x88);
+	VALIDATE(SLineInfo, field_89, 0x89);
+	VALIDATE(SLineInfo, field_8A, 0x8A);
+	VALIDATE(SLineInfo, field_8C, 0x8C);
 }
--- m3dcolij.h
+++ m3dcolij.h
@@ -6,11 +6,39 @@
 
 #include "export.h"
 #include "vector.h"
+#include "ps2funcs.h"
 
 struct SLineInfo
 {
 	CVector vec_0;
 	CVector vec_C;
+
+	int field_18;
+	int field_1C;
+	int field_20;
+	int field_24;
+	int field_28;
+	int field_2C;
+	unsigned char padAfter2C[0x40-0x2C-4];
+
+	int field_40;
+	int field_44;
+	MATRIX field_48;
+	int field_68;
+
+	int field_6C;
+	int field_70;
+	int field_74;
+	__int16 field_78;
+	__int16 field_7A;
+	__int16 field_7C;
+
+	int field_80;
+	int field_84;
+	unsigned char field_88;
+	unsigned char field_89;
+	__int16 field_8A;
+	int field_8C;
 };
 
 
