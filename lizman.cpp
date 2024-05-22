b26847f3e73ce84f6e807bc9ed00f67f28143b85
krystalgamer <callofdutypsp@gmail.com>
2024-05-22 19:39:34 +0200

    CLizMan::PlaySingleAnim and CLizMan::StandStill

--- lizman.cpp
+++ lizman.cpp
@@ -1,6 +1,31 @@
 #include "lizman.h"
 #include "validate.h"
 
+
+
+// @Ok
+void __inline CLizMan::PlaySingleAnim(__int16 a1, int a2, int a3)
+{
+	this->field_340 = 0;
+	this->RunAnim(a1, a2, a3);
+}
+
+// @Ok
+void __inline CLizMan::StandStill(void)
+{
+	switch (this->dumbAssPad)
+	{
+		case 0:
+			this->Neutralize();
+			this->PlaySingleAnim(19, 0, -1);
+			this->dumbAssPad++;
+			break;
+		case 1:
+			print_if_false(0, "Unknown substate.");
+			break;
+	}
+}
+
 void validate_CLizMan(void){
 	VALIDATE_SIZE(CLizMan, 0x3B8);
 
@@ -8,6 +33,8 @@ void validate_CLizMan(void){
 	VALIDATE(CLizMan, field_328, 0x328);
 	VALIDATE(CLizMan, field_32C, 0x32C);
 	VALIDATE(CLizMan, field_338, 0x338);
+	VALIDATE(CLizMan, field_340, 0x340);
+
 	VALIDATE(CLizMan, field_374, 0x374);
 	VALIDATE(CLizMan, field_398, 0x398);
 	VALIDATE(CLizMan, field_3AC, 0x3AC);
--- lizman.h
+++ lizman.h
@@ -5,6 +5,9 @@
 
 class CLizMan : public CBaddy {
 public:
+	EXPORT void PlaySingleAnim(__int16, int, int);
+	EXPORT void StandStill(void);
+
 	unsigned char padLizStart[0x4];
 
 	int field_328;
@@ -12,7 +15,10 @@ public:
 	unsigned char padAfter32C[0x338-0x32C-4];
 
 	int field_338;
-	unsigned char padAfter338[0x374-0x338-4];
+	unsigned char padAfter338[0x340-0x338-4];
+
+	int field_340;
+	unsigned char padAfter340[0x374-0x340-4];
 
 
 	int field_374;
