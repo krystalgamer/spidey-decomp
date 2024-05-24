877a705b39961a8f78fb1496316f08e60f9a8a90
krystalgamer <callofdutypsp@gmail.com>
2024-05-24 16:21:33 +0200

    Shatter_MaybeMakeGlassShatterSound

--- shatter.cpp
+++ shatter.cpp
@@ -1,1 +1,7 @@
 #include "shatter.h"
+
+static int gGlassShatterSound;
+void Shatter_MaybeMakeGlassShatterSound(void)
+{
+	gGlassShatterSound = 0;
+}
--- shatter.h
+++ shatter.h
@@ -3,4 +3,9 @@
 #ifndef SHATTER_H
 #define SHATTER_H
 
+#include "export.h"
+
+
+EXPORT void Shatter_MaybeMakeGlassShatterSound(void);
+
 #endif
