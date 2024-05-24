3e0f013fbe5f2ef223b693c848fb4a60a9d2390f
krystalgamer <callofdutypsp@gmail.com>
2024-05-24 17:42:19 +0200

    FileIO_Sync

--- dcfileio.cpp
+++ dcfileio.cpp
@@ -1,1 +1,11 @@
 #include "dcfileio.h"
+
+
+static volatile int gFileIOStatus;
+
+// @NotOk
+// globals
+void FileIO_Sync(void)
+{
+	while (gFileIOStatus);
+}
--- dcfileio.h
+++ dcfileio.h
@@ -3,4 +3,8 @@
 #ifndef DCFILEIO_H
 #define DCFILEIO_H
 
+#include "export.h"
+
+EXPORT void FileIO_Sync(void);
+
 #endif
