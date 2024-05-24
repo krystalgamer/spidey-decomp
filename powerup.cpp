6a6c3080a14034e3f7b0dfd5a7142ba2361f47a0
krystalgamer <callofdutypsp@gmail.com>
2024-05-24 17:01:58 +0200

    CPowerUp::SetNode

--- powerup.cpp
+++ powerup.cpp
@@ -9,7 +9,17 @@ void CPowerUp::SetGravity(int a2, int a3)
 	this->field_79 = a3;
 }
 
+// @Ok
+void CPowerUp::SetNode(int a2)
+{
+	this->field_100 = 1;
+	this->field_106 = a2;
+}
+
 void validate_CPowerUp(void)
 {
 	VALIDATE_SIZE(CPowerUp, 0x138);
+
+	VALIDATE(CPowerUp, field_100, 0x100);
+	VALIDATE(CPowerUp, field_106, 0x106);
 }
--- powerup.h
+++ powerup.h
@@ -10,7 +10,14 @@ class CPowerUp : public CBody
 {
 	public:
 		EXPORT void SetGravity(int, int);
-		unsigned char pad[0x138-0xF4];
+		EXPORT void SetNode(int);
+
+		unsigned char padTop[0x100-0xF4];
+		unsigned char field_100;
+		unsigned char padAfter100[0x106-0x100-1];
+
+		__int16 field_106;
+		unsigned char padBottom[0x138-0x106-2];
 };
 
 void validate_CPowerUp(void);
