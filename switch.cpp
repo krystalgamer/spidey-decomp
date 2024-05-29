af62af61b2c37ca0768545c2f38e29cc4bb2d26b
krystalgamer <callofdutypsp@gmail.com>
2024-05-29 19:50:21 +0200

    CSwitch::GetAutoAimTargetPointer

--- switch.cpp
+++ switch.cpp
@@ -7,9 +7,29 @@ void __inline CSwitch::SwitchInactive(void)
 	this->field_100 = 0;
 }
 
+// @Ok
+CVector* CSwitch::GetAutoAimTargetPointer(void)
+{
+	switch(this->field_100)
+	{
+		case 1:
+			return &this->field_10C;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			return &this->field_118;
+		default:
+			return NULL;
+	}
+}
+
 void validate_CSwitch(void)
 {
 	VALIDATE_SIZE(CSwitch, 0x128);
 
 	VALIDATE(CSwitch, field_100, 0x100);
+
+	VALIDATE(CSwitch, field_10C, 0x10C);
+	VALIDATE(CSwitch, field_118, 0x118);
 }
--- switch.h
+++ switch.h
@@ -10,11 +10,16 @@ class CSwitch : public CBody
 {
 	public:
 		EXPORT void SwitchInactive(void);
+		EXPORT CVector* GetAutoAimTargetPointer(void);
+
 		unsigned char padTop[0x100-0xF4];
 
 		int field_100;
+		unsigned char padAfter100[0x10C-0x100-4];
 
-		unsigned char padBottom[0x128-0x100-4];
+		CVector field_10C;
+		CVector field_118;
+		unsigned char padBottom[0x128-0x118-sizeof(CVector)];
 };
 
 void validate_CSwitch(void);
