b7cf4e19e5bd010fcfade1d36eb56be68968fbf3
krystalgamer <callofdutypsp@gmail.com>
2024-05-31 19:41:18 +0200

    CManipOb::Smash

--- manipob.cpp
+++ manipob.cpp
@@ -27,6 +27,58 @@ void CManipOb::Throw(CVector *a2)
 	this->csVector1.vx = Rnd(32) + 64;
 }
 
+// @NotOk
+// Revisit because of weird decomp
+void CManipOb::Smash(void)
+{
+	CVector v3;
+	v3.vx = 0;
+	v3.vy = 4096;
+	v3.vz = 0;
+
+	SLineInfo v4;
+	v4.vec_0.vx = 0;
+	v4.vec_0.vy = 0;
+	v4.vec_0.vz = 0;
+	v4.vec_C.vx = 0;
+	v4.vec_C.vy = 0;
+	v4.vec_C.vz = 0;
+	v4.field_18 = 0;
+	v4.field_1C = 0;
+	v4.field_20 = 0;
+	v4.field_24 = 0;
+	v4.field_28 = 0;
+	v4.field_2C = 0;
+	v4.field_6C = 0;
+	v4.field_70 = 0;
+	v4.field_74 = 0;
+	v4.field_78 = 0;
+	v4.field_7A = (__int16)0xF000;
+	v4.field_7C = 0;
+
+	this->mAccellorVel.vx = 0;
+	this->mAccellorVel.vy = 0x20000;
+	this->mAccellorVel.vz = 0x20000;
+
+	this->Chunk(&v4, &v3);
+	this->TurnOffShadow();
+	this->SendPulse();
+
+	this->Die();
+}
+
+// @TODO
+void CManipOb::Chunk(SLineInfo*, CVector*)
+{}
+
+// @TODO
+void CManipOb::TurnOffShadow(void)
+{}
+
+// @TODO
+void CManipOb::SendPulse(void)
+{}
+
 void validate_CManipOb(void)
 {
 	VALIDATE_SIZE(CManipOb, 0x128);
--- manipob.h
+++ manipob.h
@@ -5,6 +5,7 @@
 
 #include "export.h"
 #include "ob.h"
+#include "m3dcolij.h"
 
 class CManipOb : public CBody
 {
@@ -12,6 +13,11 @@ class CManipOb : public CBody
 		EXPORT void CalculateThrowPosition(CVector*, int);
 		EXPORT void ThrowPos(CVector *, int);
 		EXPORT void Throw(CVector *);
+		EXPORT void Smash(void);
+
+		EXPORT void Chunk(SLineInfo*, CVector*);
+		EXPORT void TurnOffShadow(void);
+		EXPORT void SendPulse(void);
 
 		unsigned char padTop[0x10C-0xF4];
 		int field_10C;
