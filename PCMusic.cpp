78fd0e63511314df86aaeb91a8def9a4759a4f24
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:03:19 +0200

    PCMusic placeholders

--- PCMusic.cpp
+++ PCMusic.cpp
@@ -1,1 +1,67 @@
 #include "PCMusic.h"
+
+// @SMALLTODO
+void CloseMusicFile(void)
+{
+    printf("CloseMusicFile(void)");
+}
+
+// @SMALLTODO
+void MusicThreadProc(void *)
+{
+    printf("MusicThreadProc(void *)");
+}
+
+// @SMALLTODO
+void NextFrame(void)
+{
+    printf("NextFrame(void)");
+}
+
+// @SMALLTODO
+void OpenMusicFile(char *,bool)
+{
+    printf("OpenMusicFile(char *,bool)");
+}
+
+// @SMALLTODO
+void PCMUSIC_Finish(void)
+{
+    printf("PCMUSIC_Finish(void)");
+}
+
+// @SMALLTODO
+void PCMUSIC_GetStatus(void)
+{
+    printf("PCMUSIC_GetStatus(void)");
+}
+
+// @SMALLTODO
+void PCMUSIC_Init(void)
+{
+    printf("PCMUSIC_Init(void)");
+}
+
+// @SMALLTODO
+void PCMUSIC_Pause(i32)
+{
+    printf("PCMUSIC_Pause(i32)");
+}
+
+// @SMALLTODO
+void PCMUSIC_Play(i32)
+{
+    printf("PCMUSIC_Play(i32)");
+}
+
+// @SMALLTODO
+void PCMUSIC_SetVolume(i32)
+{
+    printf("PCMUSIC_SetVolume(i32)");
+}
+
+// @SMALLTODO
+void PCMUSIC_Stop(void)
+{
+    printf("PCMUSIC_Stop(void)");
+}
--- PCMusic.h
+++ PCMusic.h
@@ -3,4 +3,19 @@
 #ifndef PCMUSIC_H
 #define PCMUSIC_H
 
+#include "export.h"
+
+EXPORT void CloseMusicFile(void);
+EXPORT void MusicThreadProc(void *);
+EXPORT void NextFrame(void);
+EXPORT void OpenMusicFile(char *,bool);
+EXPORT void PCMUSIC_Finish(void);
+EXPORT void PCMUSIC_GetStatus(void);
+EXPORT void PCMUSIC_Init(void);
+EXPORT void PCMUSIC_Pause(i32);
+EXPORT void PCMUSIC_Play(i32);
+EXPORT void PCMUSIC_SetVolume(i32);
+EXPORT void PCMUSIC_Stop(void);
+
+
 #endif
