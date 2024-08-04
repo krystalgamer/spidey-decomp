1bda5932da9e74bdce7c49907c782601da2b6e51
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:18:23 +0200

    submarin placeholders

--- submarin.cpp
+++ submarin.cpp
@@ -3,6 +3,31 @@
 #include "ps2m3d.h"
 
 
+// @SMALLTODO
+CSubmariner::CSubmariner(i16 *,i32)
+{
+    printf("CSubmariner::CSubmariner(i16 *,i32)");
+}
+
+// @SMALLTODO
+CSubmariner::~CSubmariner(void)
+{
+    printf("CSubmariner::~CSubmariner(void)");
+}
+
+// @SMALLTODO
+void Submariner_CreateSubmariner(u32 const *,u32 *)
+{
+    printf("Submariner_CreateSubmariner(ulong const *,u32 *)");
+}
+
+// @SMALLTODO
+void Submariner_RelocatableModuleClear(void)
+{
+    printf("Submariner_RelocatableModuleClear(void)");
+}
+
+// @Ok
 void CSubmariner::AI(void){
 	if (*submarinerDieRelated){
 		this->Die(0);
--- submarin.h
+++ submarin.h
@@ -1,11 +1,21 @@
+#ifndef SUBMARIN_H
+#define SUBMARIN_H
+
 #pragma once
 
 #include "baddy.h"
 #include "export.h"
 
 class CSubmariner : public CBaddy {
-	EXPORT void AI(void);
+	EXPORT CSubmariner(i16 *,i32);
+	EXPORT ~CSubmariner(void);
+
+	EXPORT virtual void AI(void);
 };
 
+EXPORT void Submariner_CreateSubmariner(u32 const *,u32 *);
+EXPORT void Submariner_RelocatableModuleClear(void);
 
 void validate_CSubmariner(void);
+
+#endif
