c184d773e642157b9c0210343540eb1571d37f5c
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:23:56 +0200

    screen.cpp placeholders

--- screen.cpp
+++ screen.cpp
@@ -3,6 +3,48 @@
 
 static bool gScreenTarget;
 
+// @SMALLTODO
+void Screen_DrawArrow(void)
+{
+    printf("Screen_DrawArrow(void)");
+}
+
+// @MEDIUMTODO
+void Screen_DrawTarget(void)
+{
+    printf("Screen_DrawTarget(void)");
+}
+
+// @MEDIUMTODO
+void Screen_SepiaFade(void)
+{
+    printf("Screen_SepiaFade(void)");
+}
+
+// @SMALLTODO
+void Screen_SetTarget(CVector *,u16,i16)
+{
+    printf("Screen_SetTarget(CVector *,u16,i16)");
+}
+
+// @SMALLTODO
+void Screen_StartCircularFadeIn(i32,i32)
+{
+    printf("Screen_StartCircularFadeIn(i32,i32)");
+}
+
+// @SMALLTODO
+void Screen_UpdateFades(void)
+{
+    printf("Screen_UpdateFades(void)");
+}
+
+// @MEDIUMTODO
+void screen_DrawCircularFade(void)
+{
+    printf("screen_DrawCircularFade(void)");
+}
+
 // @NotOk
 // Globals
 void Screen_TargetOn(bool value)
--- screen.h
+++ screen.h
@@ -4,7 +4,16 @@
 #define SCREEN_H
 
 #include "export.h"
+#include "vector.h"
 
 
 EXPORT void Screen_TargetOn(bool);
+EXPORT void Screen_DrawArrow(void);
+EXPORT void Screen_DrawTarget(void);
+EXPORT void Screen_SepiaFade(void);
+EXPORT void Screen_SetTarget(CVector *,u16,i16);
+EXPORT void Screen_StartCircularFadeIn(i32,i32);
+EXPORT void Screen_UpdateFades(void);
+EXPORT void screen_DrawCircularFade(void);
+
 #endif
