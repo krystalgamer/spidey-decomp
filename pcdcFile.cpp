b0e49414dc2711844c2ea35064d11d4988bc2be2
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 18:17:33 +0200

    pcdcFile.cpp placeholders

--- pcdcFile.cpp
+++ pcdcFile.cpp
@@ -1,1 +1,55 @@
 #include "pcdcFile.h"
+
+// @SMALLTODO
+void closeFilePKR(i32)
+{
+    printf("closeFilePKR(i32)");
+}
+
+// @SMALLTODO
+void closePKR(void)
+{
+    printf("closePKR(void)");
+}
+
+// @SMALLTODO
+void findFilePKR(char *,char const *)
+{
+    printf("findFilePKR(char *,char const *)");
+}
+
+// @SMALLTODO
+void nextFile(void)
+{
+    printf("nextFile(void)");
+}
+
+// @SMALLTODO
+void openFilePKR(char *,char const *)
+{
+    printf("openFilePKR(char *,char const *)");
+}
+
+// @SMALLTODO
+void openPKR(void)
+{
+    printf("openPKR(void)");
+}
+
+// @SMALLTODO
+void readFilePKR(i32,char *,i32)
+{
+    printf("readFilePKR(i32,char *,i32)");
+}
+
+// @SMALLTODO
+void seekFilePKR(i32,i32,i32)
+{
+    printf("seekFilePKR(i32,i32,i32)");
+}
+
+// @SMALLTODO
+void tellFilePKR(i32)
+{
+    printf("tellFilePKR(i32)");
+}
--- pcdcFile.h
+++ pcdcFile.h
@@ -3,4 +3,15 @@
 #ifndef PCDCFILE_H
 #define PCDCFILE_H
 
+#include "export.h"
+EXPORT void closeFilePKR(i32);
+EXPORT void closePKR(void);
+EXPORT void findFilePKR(char *,char const *);
+EXPORT void nextFile(void);
+EXPORT void openFilePKR(char *,char const *);
+EXPORT void openPKR(void);
+EXPORT void readFilePKR(i32,char *,i32);
+EXPORT void seekFilePKR(i32,i32,i32);
+EXPORT void tellFilePKR(i32);
+
 #endif
