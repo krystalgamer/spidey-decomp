ccda3b789ce370b2d99c2abb392180e24650530b
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 18:33:42 +0200

    vram placeholders

--- vram.cpp
+++ vram.cpp
@@ -1,1 +1,13 @@
 #include "vram.h"
+
+// @SMALLTODO
+void VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32)
+{
+    printf("VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32)");
+}
+
+// @SMALLTODO
+void VRAMRectUnpack(tagSVRAMRect *)
+{
+    printf("VRAMRectUnpack(tagSVRAMRect *)");
+}
--- vram.h
+++ vram.h
@@ -3,4 +3,12 @@
 #ifndef VRAM_H
 #define VRAM_H
 
+#include "export.h"
+
+// @FIXME
+#define tagSVRAMRect i32
+
+EXPORT void VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32);
+EXPORT void VRAMRectUnpack(tagSVRAMRect *);
+
 #endif
