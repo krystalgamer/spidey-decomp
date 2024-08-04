530e3a4e11f2640446b81fab4d233af3b85f8869
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:20:00 +0200

    spid_ai0.cpp placeholder

--- spid_ai0.cpp
+++ spid_ai0.cpp
@@ -1,1 +1,8 @@
 #include "spid_ai0.h"
+
+// @MEDIUMTODO
+void SpideyAI0(CPlayer *)
+{
+    printf("SpideyAI0(CPlayer *)");
+}
+
--- spid_ai0.h
+++ spid_ai0.h
@@ -3,4 +3,9 @@
 #ifndef SPID_AI0_H
 #define SPID_AI0_H
 
+#include "spidey.h"
+#include "export.h"
+
+EXPORT void SpideyAI0(CPlayer *);
+
 #endif
