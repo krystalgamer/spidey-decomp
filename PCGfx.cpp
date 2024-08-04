8ec88762600311ba601debe801b6f288299659da
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 18:15:27 +0200

    PCGfx.cpp placeholders

--- PCGfx.cpp
+++ PCGfx.cpp
@@ -1,1 +1,193 @@
 #include "PCGfx.h"
+
+// @SMALLTODO
+void PCGfx_BeginScene(u32,i32)
+{
+    printf("PCGfx_BeginScene(u32,i32)");
+}
+
+// @MEDIUMTODO
+void PCGfx_ClipSendIndexedVertList(tagKMVERTEX3 const *,i32,u16 const *,i32)
+{
+    printf("PCGfx_ClipSendIndexedVertList(tagKMVERTEX3 const *,i32,u16 const *,i32)");
+}
+
+// @SMALLTODO
+void PCGfx_ClipTriToNearPlane(_DXVERT **,_DXVERT *const *)
+{
+    printf("PCGfx_ClipTriToNearPlane(_DXVERT **,_DXVERT *const *)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DoModelPreview(void)
+{
+    printf("PCGfx_DoModelPreview(void)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawLine(float,float,float,u32,float,float,float,u32,float)
+{
+    printf("PCGfx_DrawLine(float,float,float,u32,float,float,float,u32,float)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawQPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float)
+{
+    printf("PCGfx_DrawQPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawQPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32)
+{
+    printf("PCGfx_DrawQPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawQuad2D(float,float,float,float,float,float,float,float,u32,float,bool)
+{
+    printf("PCGfx_DrawQuad2D(float,float,float,float,float,float,float,float,u32,float,bool)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawTPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float)
+{
+    printf("PCGfx_DrawTPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawTPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32)
+{
+    printf("PCGfx_DrawTPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32)");
+}
+
+// @MEDIUMTODO
+void PCGfx_DrawTexture2D(i32,i32,i32,float,u32,u32,float)
+{
+    printf("PCGfx_DrawTexture2D(i32,i32,i32,float,u32,u32,float)");
+}
+
+// @MEDIUMTODO
+void PCGfx_EndScene(i32)
+{
+    printf("PCGfx_EndScene(i32)");
+}
+
+// @SMALLTODO
+void PCGfx_Exit(void)
+{
+    printf("PCGfx_Exit(void)");
+}
+
+// @SMALLTODO
+void PCGfx_GetZLayerFurthest(void)
+{
+    printf("PCGfx_GetZLayerFurthest(void)");
+}
+
+// @SMALLTODO
+void PCGfx_GetZLayerNearest(void)
+{
+    printf("PCGfx_GetZLayerNearest(void)");
+}
+
+// @SMALLTODO
+void PCGfx_IncZLayerFurthest(void)
+{
+    printf("PCGfx_IncZLayerFurthest(void)");
+}
+
+// @SMALLTODO
+void PCGfx_IncZLayerNearest(void)
+{
+    printf("PCGfx_IncZLayerNearest(void)");
+}
+
+// @SMALLTODO
+void PCGfx_InitAtStart(void)
+{
+    printf("PCGfx_InitAtStart(void)");
+}
+
+// @SMALLTODO
+void PCGfx_IsInScene(void)
+{
+    printf("PCGfx_IsInScene(void)");
+}
+
+// @SMALLTODO
+void PCGfx_ProcessTexture(_tagKMSTRIPHEAD *,i32,DCGfx_BlendingMode)
+{
+    printf("PCGfx_ProcessTexture(_tagKMSTRIPHEAD *,i32,DCGfx_BlendingMode)");
+}
+
+// @SMALLTODO
+void PCGfx_RenderInit(float,float,float,float,float)
+{
+    printf("PCGfx_RenderInit(float,float,float,float,float)");
+}
+
+// @SMALLTODO
+void PCGfx_RenderModelPreview(void *,char const *,i32)
+{
+    printf("PCGfx_RenderModelPreview(void *,char const *,i32)");
+}
+
+// @SMALLTODO
+void PCGfx_SetBrightness(i32)
+{
+    printf("PCGfx_SetBrightness(i32)");
+}
+
+// @SMALLTODO
+void PCGfx_SetFogParams(float,float,u32)
+{
+    printf("PCGfx_SetFogParams(float,float,u32)");
+}
+
+// @SMALLTODO
+void PCGfx_SetRenderParameter(DCGfx_RenderParameter,DCGfx_RenderSetting)
+{
+    printf("PCGfx_SetRenderParameter(DCGfx_RenderParameter,DCGfx_RenderSetting)");
+}
+
+// @SMALLTODO
+void PCGfx_SetSkyColor(u32)
+{
+    printf("PCGfx_SetSkyColor(u32)");
+}
+
+// @SMALLTODO
+void PCGfx_UseTexture(i32,DCGfx_BlendingMode)
+{
+    printf("PCGfx_UseTexture(i32,DCGfx_BlendingMode)");
+}
+
+// @MEDIUMTODO
+void PCPanel_DrawTexturedPoly(float,Texture const *,i32,i32,i32,i32,u8)
+{
+    printf("PCPanel_DrawTexturedPoly(float,Texture const *,i32,i32,i32,i32,u8)");
+}
+
+// @MEDIUMTODO
+void ZCLIP_VERT(_DXVERT *,_DXVERT *,_DXVERT *,float)
+{
+    printf("ZCLIP_VERT(_DXVERT *,_DXVERT *,_DXVERT *,float)");
+}
+
+// @SMALLTODO
+void createSuperItem(CItem *)
+{
+    printf("createSuperItem(CItem *)");
+}
+
+// @SMALLTODO
+void setupFog(void)
+{
+    printf("setupFog(void)");
+}
+
+// @MEDIUMTODO
+void submitPoly(_DXVERT **,i32)
+{
+    printf("submitPoly(_DXVERT **,i32)");
+}
--- PCGfx.h
+++ PCGfx.h
@@ -3,4 +3,50 @@
 #ifndef PCGFX_H
 #define PCGFX_H
 
+#include "ob.h"
+#include "texture.h"
+#include "export.h"
+
+// @FIXME
+#define _DXVERT i32
+#define tagKMVERTEX3 i32
+#define DCGfx_RenderParameter i32
+#define DCGfx_RenderSetting i32
+#define _tagKMSTRIPHEAD i32
+#define DCGfx_BlendingMode i32
+
+EXPORT void PCGfx_BeginScene(u32,i32);
+EXPORT void PCGfx_ClipSendIndexedVertList(tagKMVERTEX3 const *,i32,u16 const *,i32);
+EXPORT void PCGfx_ClipTriToNearPlane(_DXVERT **,_DXVERT *const *);
+EXPORT void PCGfx_DoModelPreview(void);
+EXPORT void PCGfx_DrawLine(float,float,float,u32,float,float,float,u32,float);
+EXPORT void PCGfx_DrawQPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float
+,float,float,float,u32,float);
+EXPORT void PCGfx_DrawQPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32);
+EXPORT void PCGfx_DrawQuad2D(float,float,float,float,float,float,float,float,u32,float,bool);
+EXPORT void PCGfx_DrawTPoly2D(float,float,float,float,u32,float,float,float,float,u32,float,float,float,float,u32,float);
+EXPORT void PCGfx_DrawTPoly3D(float,float,float,float,float,u32,float,float,float,float,float,u32,float,float,float,float,float,u32);
+EXPORT void PCGfx_DrawTexture2D(i32,i32,i32,float,u32,u32,float);
+EXPORT void PCGfx_EndScene(i32);
+EXPORT void PCGfx_Exit(void);
+EXPORT void PCGfx_GetZLayerFurthest(void);
+EXPORT void PCGfx_GetZLayerNearest(void);
+EXPORT void PCGfx_IncZLayerFurthest(void);
+EXPORT void PCGfx_IncZLayerNearest(void);
+EXPORT void PCGfx_InitAtStart(void);
+EXPORT void PCGfx_IsInScene(void);
+EXPORT void PCGfx_ProcessTexture(_tagKMSTRIPHEAD *,i32,DCGfx_BlendingMode);
+EXPORT void PCGfx_RenderInit(float,float,float,float,float);
+EXPORT void PCGfx_RenderModelPreview(void *,char const *,i32);
+EXPORT void PCGfx_SetBrightness(i32);
+EXPORT void PCGfx_SetFogParams(float,float,u32);
+EXPORT void PCGfx_SetRenderParameter(DCGfx_RenderParameter,DCGfx_RenderSetting);
+EXPORT void PCGfx_SetSkyColor(u32);
+EXPORT void PCGfx_UseTexture(i32,DCGfx_BlendingMode);
+EXPORT void PCPanel_DrawTexturedPoly(float,Texture const *,i32,i32,i32,i32,u8);
+EXPORT void ZCLIP_VERT(_DXVERT *,_DXVERT *,_DXVERT *,float);
+EXPORT void createSuperItem(CItem *);
+EXPORT void setupFog(void);
+EXPORT void submitPoly(_DXVERT **,i32);
+
 #endif
