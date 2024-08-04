14f0e3dab5f744cf636448d0cf07e3462aae6d9e
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:53:26 +0200

    crate placeholders

--- crate.cpp
+++ crate.cpp
@@ -1,1 +1,7 @@
 #include "crate.h"
+
+// @SMALLTODO
+void Crate_Destroy(CItem *)
+{
+    printf("Crate_Destroy(CItem *)");
+}
--- crate.h
+++ crate.h
@@ -3,4 +3,9 @@
 #ifndef CRATE_H
 #define CRATE_H
 
+#include "export.h"
+#include "ob.h"
+
+EXPORT void Crate_Destroy(CItem *);
+
 #endif
