4e055e07ce17ee377b4690bafb90010f3a559180
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:28:17 +0200

    post.cpp placeholders

--- post.cpp
+++ post.cpp
@@ -1,1 +1,37 @@
 #include "post.h"
+
+// @SMALLTODO
+void Post_DoPauseDisplayListProcessing(void)
+{
+    printf("Post_DoPauseDisplayListProcessing(void)");
+}
+
+// @SMALLTODO
+void Post_DoPausePaletteProcessing(void)
+{
+    printf("Post_DoPausePaletteProcessing(void)");
+}
+
+// @SMALLTODO
+void Post_PostProcessEffects(void)
+{
+    printf("Post_PostProcessEffects(void)");
+}
+
+// @MEDIUMTODO
+void Post_SpideyLogo(void)
+{
+    printf("Post_SpideyLogo(void)");
+}
+
+// @SMALLTODO
+void Post_UndoPausePaletteProcessing(void)
+{
+    printf("Post_UndoPausePaletteProcessing(void)");
+}
+
+// @SMALLTODO
+void Post_WaterEffect(void)
+{
+    printf("Post_WaterEffect(void)");
+}
--- post.h
+++ post.h
@@ -3,4 +3,13 @@
 #ifndef POST_H
 #define POST_H
 
+#include "export.h"
+
+EXPORT void Post_DoPauseDisplayListProcessing(void);
+EXPORT void Post_DoPausePaletteProcessing(void);
+EXPORT void Post_PostProcessEffects(void);
+EXPORT void Post_SpideyLogo(void);
+EXPORT void Post_UndoPausePaletteProcessing(void);
+EXPORT void Post_WaterEffect(void);
+
 #endif
