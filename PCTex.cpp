365569be6173f350a5bf333dcc5f7b7ba853d2ca
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 18:09:01 +0200

    PCTex.cpp placeholders

--- PCTex.cpp
+++ PCTex.cpp
@@ -1,7 +1,223 @@
 #include "PCTex.h"
 #include "validate.h"
 
-static WeirdTextureHolder gGlobalTextures[2];
+EXPORT WeirdTextureHolder gGlobalTextures[2];
+
+// @SMALLTODO
+void CheckValidTexture(i32)
+{
+    printf("CheckValidTexture(i32)");
+}
+
+// @MEDIUMTODO
+void ConvertPSXPaletteToPC(u16 const *,u16 *,u32,u32)
+{
+    printf("ConvertPSXPaletteToPC(u16 const *,u16 *,u32,u32)");
+}
+
+// @MEDIUMTODO
+void PCTEX_Init(void)
+{
+    printf("PCTEX_Init(void)");
+}
+
+// @SMALLTODO
+void PCTex_BufferPVR(char const *,char *)
+{
+    printf("PCTex_BufferPVR(char const *,char *)");
+}
+
+// @SMALLTODO
+void PCTex_CountActiveTextures(void)
+{
+    printf("PCTex_CountActiveTextures(void)");
+}
+
+// @SMALLTODO
+void PCTex_CreateClut(i32)
+{
+    printf("PCTex_CreateClut(i32)");
+}
+
+// @MEDIUMTODO
+void PCTex_CreateTexture16(i32,i32,void const *,u16 const *,char const *,i32,i32,u32)
+{
+    printf("PCTex_CreateTexture16(i32,i32,void const *,u16 const *,char const *,i32,i32,u32)");
+}
+
+// @MEDIUMTODO
+void PCTex_CreateTexture256(i32,i32,void const *,u16 const *,u32,char const *,i32,i32)
+{
+    printf("PCTex_CreateTexture256(i32,i32,void const *,u16 const *,u32,char const *,i32,i32)");
+}
+
+// @SMALLTODO
+void PCTex_CreateTexturePVR(i32,i32,u32,void const *,u32,char const *,u32)
+{
+    printf("PCTex_CreateTexturePVR(i32,i32,u32,void const *,u32,char const *,u32)");
+}
+
+// @MEDIUMTODO
+void PCTex_CreateTexturePVRInId(i32,i32,i32,u32,void const *,u32,char const *,u32)
+{
+    printf("PCTex_CreateTexturePVRInId(i32,i32,i32,u32,void const *,u32,char const *,u32)");
+}
+
+// @SMALLTODO
+void PCTex_FindUnusedTextureId(void)
+{
+    printf("PCTex_FindUnusedTextureId(void)");
+}
+
+// @SMALLTODO
+void PCTex_FreePcIcons(void)
+{
+    printf("PCTex_FreePcIcons(void)");
+}
+
+// @SMALLTODO
+void PCTex_GetDirect3DTexture(i32)
+{
+    printf("PCTex_GetDirect3DTexture(i32)");
+}
+
+// @SMALLTODO
+void PCTex_GetInvTextureSize(i32,float *,float *)
+{
+    printf("PCTex_GetInvTextureSize(i32,float *,float *)");
+}
+
+// @SMALLTODO
+void PCTex_GetTextureSize(i32,i32 *,i32 *)
+{
+    printf("PCTex_GetTextureSize(i32,i32 *,i32 *)");
+}
+
+// @SMALLTODO
+void PCTex_GetTextureSplitCount(i32)
+{
+    printf("PCTex_GetTextureSplitCount(i32)");
+}
+
+// @SMALLTODO
+void PCTex_InitSystemTextures(void)
+{
+    printf("PCTex_InitSystemTextures(void)");
+}
+
+// @MEDIUMTODO
+void PCTex_LoadLtiTexture(char const *,u32,i32,u32)
+{
+    printf("PCTex_LoadLtiTexture(char const *,u32,i32,u32)");
+}
+
+// @SMALLTODO
+void PCTex_LoadPcIcons(void)
+{
+    printf("PCTex_LoadPcIcons(void)");
+}
+
+// @SMALLTODO
+void PCTex_LoadTexturePVR(char const *,char *)
+{
+    printf("PCTex_LoadTexturePVR(char const *,char *)");
+}
+
+// @SMALLTODO
+void PCTex_ReleaseAllTextures(void)
+{
+    printf("PCTex_ReleaseAllTextures(void)");
+}
+
+// @SMALLTODO
+void PCTex_ReleaseSysTexture(i32,bool)
+{
+    printf("PCTex_ReleaseSysTexture(i32,bool)");
+}
+
+// @SMALLTODO
+void PCTex_ReleaseTexture(i32,bool)
+{
+    printf("PCTex_ReleaseTexture(i32,bool)");
+}
+
+// @MEDIUMTODO
+void PCTex_ReloadTextures(void)
+{
+    printf("PCTex_ReloadTextures(void)");
+}
+
+// @SMALLTODO
+void PCTex_TextureHasAlpha(i32)
+{
+    printf("PCTex_TextureHasAlpha(i32)");
+}
+
+// @SMALLTODO
+void PCTex_UnbufferPVR(PVRHeader *)
+{
+    printf("PCTex_UnbufferPVR(PVRHeader *)");
+}
+
+// @SMALLTODO
+void PCTex_UnloadTextures(void)
+{
+    printf("PCTex_UnloadTextures(void)");
+}
+
+// @SMALLTODO
+void PCTex_UpdateForSoftwareRenderer(void)
+{
+    printf("PCTex_UpdateForSoftwareRenderer(void)");
+}
+
+// @SMALLTODO
+void clutToClutPc(u16 const *)
+{
+    printf("clutToClutPc(u16 const *)");
+}
+
+// @SMALLTODO
+void copyBitmap(void const *,i32,void *,i32,i32,i32,i32)
+{
+    printf("copyBitmap(void const *,i32,void *,i32,i32,i32,i32)");
+}
+
+// @MEDIUMTODO
+void copyConvertBitmap(void const *,i32,i32,void *,i32,i32,i32,i32,bool)
+{
+    printf("copyConvertBitmap(void const *,i32,i32,void *,i32,i32,i32,i32,bool)");
+}
+
+// @SMALLTODO
+void countLeadingZeroBits(u32)
+{
+    printf("countLeadingZeroBits(u32)");
+}
+
+// @MEDIUMTODO
+void downloadTexture(PCTexture *,u16 *,i32,i32)
+{
+    printf("downloadTexture(PCTexture *,u16 *,i32,i32)");
+}
+
+// @SMALLTODO
+void enumPixelFormatsCB(_DDPIXELFORMAT *,void *)
+{
+    printf("enumPixelFormatsCB(_DDPIXELFORMAT *,void *)");
+}
+
+// @SMALLTODO
+void releaseClutPc(_ClutPC *)
+{
+    printf("releaseClutPc(_ClutPC *)");
+}
+
+// @SMALLTODO
+void shouldForceBlend(u16 *,i32,i32,i32)
+{
+    printf("shouldForceBlend(u16 *,i32,i32,i32)");
+}
 
 // @NotOk
 // globals
--- PCTex.h
+++ PCTex.h
@@ -6,6 +6,15 @@
 #include "export.h"
 #include "bitmap256.h"
 
+// @FIXME
+#define _ClutPC i32
+// @FIXME
+#define PVRHeader i32
+// @FIXME
+#define PCTexture i32
+// @FIXME
+#define _DDPIXELFORMAT i32
+
 struct WeirdTextureHolder
 {
 	Bitmap256* texture;
@@ -23,6 +32,43 @@ EXPORT int PCTex_GetTextureFlags(int);
 EXPORT int countBits(unsigned int);
 EXPORT int countLeadingBits(unsigned int);
 EXPORT int PCTex_GetTextureSplitID(int, int);
+
+EXPORT void CheckValidTexture(i32);
+EXPORT void ConvertPSXPaletteToPC(u16 const *,u16 *,u32,u32);
+EXPORT void PCTEX_Init(void);
+EXPORT void PCTex_BufferPVR(char const *,char *);
+EXPORT void PCTex_CountActiveTextures(void);
+EXPORT void PCTex_CreateClut(i32);
+EXPORT void PCTex_CreateTexture16(i32,i32,void const *,u16 const *,char const *,i32,i32,u32);
+EXPORT void PCTex_CreateTexture256(i32,i32,void const *,u16 const *,u32,char const *,i32,i32);
+EXPORT void PCTex_CreateTexturePVR(i32,i32,u32,void const *,u32,char const *,u32);
+EXPORT void PCTex_CreateTexturePVRInId(i32,i32,i32,u32,void const *,u32,char const *,u32);
+EXPORT void PCTex_FindUnusedTextureId(void);
+EXPORT void PCTex_FreePcIcons(void);
+EXPORT void PCTex_GetDirect3DTexture(i32);
+EXPORT void PCTex_GetInvTextureSize(i32,float *,float *);
+EXPORT void PCTex_GetTextureSize(i32,i32 *,i32 *);
+EXPORT void PCTex_GetTextureSplitCount(i32);
+EXPORT void PCTex_InitSystemTextures(void);
+EXPORT void PCTex_LoadLtiTexture(char const *,u32,i32,u32);
+EXPORT void PCTex_LoadPcIcons(void);
+EXPORT void PCTex_LoadTexturePVR(char const *,char *);
+EXPORT void PCTex_ReleaseAllTextures(void);
+EXPORT void PCTex_ReleaseSysTexture(i32,bool);
+EXPORT void PCTex_ReleaseTexture(i32,bool);
+EXPORT void PCTex_ReloadTextures(void);
+EXPORT void PCTex_TextureHasAlpha(i32);
+EXPORT void PCTex_UnbufferPVR(PVRHeader *);
+EXPORT void PCTex_UnloadTextures(void);
+EXPORT void PCTex_UpdateForSoftwareRenderer(void);
+EXPORT void clutToClutPc(u16 const *);
+EXPORT void copyBitmap(void const *,i32,void *,i32,i32,i32,i32);
+EXPORT void copyConvertBitmap(void const *,i32,i32,void *,i32,i32,i32,i32,bool);
+EXPORT void countLeadingZeroBits(u32);
+EXPORT void downloadTexture(PCTexture *,u16 *,i32,i32);
+EXPORT void enumPixelFormatsCB(_DDPIXELFORMAT *,void *);
+EXPORT void releaseClutPc(_ClutPC *);
+EXPORT void shouldForceBlend(u16 *,i32,i32,i32);
 
 void validate_WeirdTextureHolder(void);
 
