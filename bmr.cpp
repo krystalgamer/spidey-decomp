6151fa12cef0cc054a7e0619c111d6a49366c1b2
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:57:53 +0200

    bmr placeholders

--- bmr.cpp
+++ bmr.cpp
@@ -1,1 +1,25 @@
 #include "bmr.h"
+
+// @SMALLTODO
+void BMP_Draw(char const *)
+{
+    printf("BMP_Draw(char const *)");
+}
+
+// @SMALLTODO
+void DeleteBMP(void)
+{
+    printf("DeleteBMP(void)");
+}
+
+// @SMALLTODO
+void DrawBMP(void)
+{
+    printf("DrawBMP(void)");
+}
+
+// @SMALLTODO
+void LoadBMP(char const *)
+{
+    printf("LoadBMP(char const *)");
+}
--- bmr.h
+++ bmr.h
@@ -3,4 +3,11 @@
 #ifndef BMR_H
 #define BMR_H
 
+#include "export.h"
+
+EXPORT void BMP_Draw(char const *);
+EXPORT void DeleteBMP(void);
+EXPORT void DrawBMP(void);
+EXPORT void LoadBMP(char const *);
+
 #endif
