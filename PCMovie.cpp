e2c5702d883d4232927ac2101b2f654a1d80b10d
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 18:03:11 +0200

    PCMovie.cpp placeholders

--- PCMovie.cpp
+++ PCMovie.cpp
@@ -1,1 +1,97 @@
 #include "PCMovie.h"
+
+// @SMALLTODO
+void CloseMovieFile(void)
+{
+    printf("CloseMovieFile(void)");
+}
+
+// @SMALLTODO
+void CreateMovieSurface(void)
+{
+    printf("CreateMovieSurface(void)");
+}
+
+// @MEDIUMTODO
+void NextMovieFrame(void)
+{
+    printf("NextMovieFrame(void)");
+}
+
+// @MEDIUMTODO
+void OpenMovieFile(char *,bool)
+{
+    printf("OpenMovieFile(char *,bool)");
+}
+
+// @SMALLTODO
+void PCMOVIE_ClosePKR(void)
+{
+    printf("PCMOVIE_ClosePKR(void)");
+}
+
+// @SMALLTODO
+void PCMOVIE_Init(void)
+{
+    printf("PCMOVIE_Init(void)");
+}
+
+// @SMALLTODO
+void PCMOVIE_InitOnce(void)
+{
+    printf("PCMOVIE_InitOnce(void)");
+}
+
+// @SMALLTODO
+void PCMOVIE_NextFrame(void)
+{
+    printf("PCMOVIE_NextFrame(void)");
+}
+
+// @SMALLTODO
+void PCMOVIE_OpenPKR(void)
+{
+    printf("PCMOVIE_OpenPKR(void)");
+}
+
+// @SMALLTODO
+void PCMOVIE_Play(char *,i32)
+{
+    printf("PCMOVIE_Play(char *,i32)");
+}
+
+// @SMALLTODO
+void PCMOVIE_SetVolume(i32)
+{
+    printf("PCMOVIE_SetVolume(i32)");
+}
+
+// @SMALLTODO
+void PCMOVIE_Stop(void)
+{
+    printf("PCMOVIE_Stop(void)");
+}
+
+// @SMALLTODO
+void findCD(void)
+{
+    printf("findCD(void)");
+}
+
+// @SMALLTODO
+void findFile(char *)
+{
+    printf("findFile(char *)");
+}
+
+// @SMALLTODO
+void findFileOffsetPKR(char *,char const *)
+{
+    printf("findFileOffsetPKR(char *,char const *)");
+}
+
+// @SMALLTODO
+void tryOpenFile(char *,char *)
+{
+    printf("tryOpenFile(char *,char *)");
+}
--- PCMovie.h
+++ PCMovie.h
@@ -3,4 +3,23 @@
 #ifndef PCMOVIE_H
 #define PCMOVIE_H
 
+#include "export.h"
+
+EXPORT void CloseMovieFile(void);
+EXPORT void CreateMovieSurface(void);
+EXPORT void NextMovieFrame(void);
+EXPORT void OpenMovieFile(char *,bool);
+EXPORT void PCMOVIE_ClosePKR(void);
+EXPORT void PCMOVIE_Init(void);
+EXPORT void PCMOVIE_InitOnce(void);
+EXPORT void PCMOVIE_NextFrame(void);
+EXPORT void PCMOVIE_OpenPKR(void);
+EXPORT void PCMOVIE_Play(char *,i32);
+EXPORT void PCMOVIE_SetVolume(i32);
+EXPORT void PCMOVIE_Stop(void);
+EXPORT void findCD(void);
+EXPORT void findFile(char *);
+EXPORT void findFileOffsetPKR(char *,char const *);
+EXPORT void tryOpenFile(char *,char *);
+
 #endif
