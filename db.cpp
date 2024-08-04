2406b6e89d15aa715ae251404a93dcea48dac335
krystalgamer <callofdutypsp@gmail.com>
2024-08-04 17:52:50 +0200

    db.cpp placeholders

--- db.cpp
+++ db.cpp
@@ -1,1 +1,37 @@
 #include "db.h"
+
+// @SMALLTODO
+void Db_CreateOTsAndPolyBuffers(void)
+{
+    printf("Db_CreateOTsAndPolyBuffers(void)");
+}
+
+// @SMALLTODO
+void Db_DefaultScreenOffsets(void)
+{
+    printf("Db_DefaultScreenOffsets(void)");
+}
+
+// @SMALLTODO
+void Db_DeleteOTsAndPolyBuffers(void)
+{
+    printf("Db_DeleteOTsAndPolyBuffers(void)");
+}
+
+// @SMALLTODO
+void Db_FlipClear(void)
+{
+    printf("Db_FlipClear(void)");
+}
+
+// @SMALLTODO
+void Db_Init(void)
+{
+    printf("Db_Init(void)");
+}
+
+// @SMALLTODO
+void Db_UpdateSky(void)
+{
+    printf("Db_UpdateSky(void)");
+}
--- db.h
+++ db.h
@@ -3,4 +3,13 @@
 #ifndef DB_H
 #define DB_H
 
+#include "export.h"
+
+EXPORT void Db_CreateOTsAndPolyBuffers(void);
+EXPORT void Db_DefaultScreenOffsets(void);
+EXPORT void Db_DeleteOTsAndPolyBuffers(void);
+EXPORT void Db_FlipClear(void);
+EXPORT void Db_Init(void);
+EXPORT void Db_UpdateSky(void);
+
 #endif
