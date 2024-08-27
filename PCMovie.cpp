64117b831b2e7ee65133b22b125326b7cf5f92ef
krystalgamer <callofdutypsp@gmail.com>
2024-08-27 16:30:13 +0100

    PCMOVIE_InitOnce

--- PCMovie.cpp
+++ PCMovie.cpp
@@ -1,7 +1,12 @@
 #include "PCMovie.h"
 #include "pkr.h"
+#include "non_win32.h"
 
 EXPORT LIBPKR_HANDLE* gMediaPkr;
+
+EXPORT char gMovieCurrentDirectory[0x200];
+EXPORT u8 gFoundMediaPkr;
+
 
 // @SMALLTODO
 void CloseMovieFile(void)
@@ -52,10 +57,64 @@ void PCMOVIE_Init(void)
     printf("PCMOVIE_Init(void)");
 }
 
-// @SMALLTODO
+EXPORT char gCdPath[0x100];
+
+// @Ok
+// @Matching
 void PCMOVIE_InitOnce(void)
 {
-    printf("PCMOVIE_InitOnce(void)");
+	CHAR RootPathName[32];
+	char rootCdPath[0x80];
+	char mediaPkrPath[0x100];
+
+	char v0 = 'C';
+	strcpy(RootPathName, "C:");
+	strcpy(gCdPath, RootPathName);
+
+	while (1)
+	{
+		RootPathName[0] = v0;
+		if (GetDriveTypeA(RootPathName) == 5)
+		{
+			strcpy(rootCdPath, RootPathName);
+			strcat(rootCdPath, "\\Setup");
+
+			strcpy(mediaPkrPath, rootCdPath);
+			strcat(mediaPkrPath, "\\");
+			strcat(mediaPkrPath, "SpideyPC.exe");
+			FILE* v1 = fopen(mediaPkrPath, "rb");
+			if (v1)
+			{
+				fclose(v1);
+				strcpy(gCdPath, rootCdPath);
+				printf_fancy("CDPATH: %s\r\n", gCdPath);
+				break;
+			}
+		}
+
+		v0++;
+		if (v0 < 'Z')
+			continue;
+
+		printf_fancy("CDPATH: %s - NOT FOUND\r\n", gCdPath);
+		break;
+	}
+
+	GetCurrentDirectoryA(0x200u, gMovieCurrentDirectory);
+	strcpy(mediaPkrPath, gMovieCurrentDirectory);
+	strcat(mediaPkrPath, "\\");
+	strcat(mediaPkrPath, "Media.pkr");
+	FILE* mediaFp = fopen(mediaPkrPath, "rb");
+
+	if (mediaFp)
+	{
+		fclose(mediaFp);
+		gFoundMediaPkr = 1;
+	}
+	else
+	{
+		gFoundMediaPkr = 0;
+	}
 }
 
 // @SMALLTODO
