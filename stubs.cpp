cb3979fff257e6156c91f9609180fd49982cc536
krystalgamer <callofdutypsp@gmail.com>
2024-08-27 16:51:02 +0100

    syRtcInit

--- stubs.cpp
+++ stubs.cpp
@@ -1,1 +1,8 @@
 #include "stubs.h"
+
+// @Ok
+// @Matching
+i32 syRtcInit(void)
+{
+	return 0;
+}
--- stubs.h
+++ stubs.h
@@ -3,4 +3,8 @@
 #ifndef STUBS_H
 #define STUBS_H
 
+#include "export.h"
+
+EXPORT i32 syRtcInit(void);
+
 #endif
