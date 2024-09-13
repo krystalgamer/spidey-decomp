e8a0dc100be8ec6166ea0527b3926a131182e8ae
krystalgamer <callofdutypsp@gmail.com>
2024-09-13 19:36:32 +0200

    buInit

--- pcdcBkup.cpp
+++ pcdcBkup.cpp
@@ -1,1 +1,15 @@
 #include "pcdcBkup.h"
+#include "non_win32.h"
+
+EXPORT CHAR gMemorycardPath[688];
+
+// @Ok
+// @Matching
+i32 buInit(i32, i32, void*, void (*a4)(void))
+{
+	GetCurrentDirectoryA(0x104u, gMemorycardPath);
+	strcat(gMemorycardPath, "\\save\\");
+	CreateDirectoryA(gMemorycardPath, 0);
+	a4();
+	return 0;
+}
