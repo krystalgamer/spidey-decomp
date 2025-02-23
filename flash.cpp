9a661295c081320d4ce93b12024d9269cc613e08
krystalgamer <callofdutypsp@gmail.com>
2025-02-23 16:30:33 +0100

    Flash_Update

--- flash.cpp
+++ flash.cpp
@@ -5,6 +5,14 @@ EXPORT i32 FlashCountdown;
 EXPORT i32 Fading;
 EXPORT u8 CurrentImportance;
 
+EXPORT u32 CurrentR;
+EXPORT u32 CurrentG;
+EXPORT u32 CurrentB;
+
+EXPORT u32 dR;
+EXPORT u32 dG;
+EXPORT u32 dB;
+
 // @Ok
 i32 Flash_FadeFinished(void)
 {
@@ -33,8 +41,39 @@ void Flash_Screen(u8,u8,u8,i32,u8,i32)
     printf("Flash_Screen(u8,u8,u8,i32,u8,i32)");
 }
 
-// @SMALLTODO
+// @Ok
+// @Matching
 void Flash_Update(void)
 {
-    printf("Flash_Update(void)");
+	if (Fading)
+	{
+		if (FadeCountdown)
+		{
+			if (--FadeCountdown == 0)
+			{
+				CurrentB = 0xFF0000;
+				CurrentG = 0xFF0000;
+				CurrentR = 0xFF0000;
+			}
+			else
+			{
+				CurrentR += dR;
+				CurrentG += dR;
+				CurrentB += dR;
+			}
+		}
+	}
+	else if (FlashCountdown)
+	{
+		if (--FlashCountdown == 0)
+		{
+			CurrentImportance = 0;
+		}
+		else
+		{
+			CurrentR -= dR;
+			CurrentG -= dG;
+			CurrentB -= dB;
+		}
+	}
 }
