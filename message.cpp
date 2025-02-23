2e1f1913d5b268c9d3fd4c38bd6c03974e3166bc
krystalgamer <callofdutypsp@gmail.com>
2025-02-23 17:35:35 +0100

    CMessage::CMessage

--- message.cpp
+++ message.cpp
@@ -3,14 +3,53 @@
 
 #include "validate.h"
 
-// @MEDIUMTODO
+// @Ok
 CMessage::CMessage(
 		CBaddy* a2,
-		CBaddy*,
+		CBaddy* pOwner,
 		i32 a4,
-		SMessageData*)
+		SMessageData* pData)
 {
-	printf("CMessage::CMessage(CBaddy*, CBaddy*, int a4, SMessageData*)");
+	for (i32 i = 0; i < 2; i++)
+	{
+		this->mVects[i][0] = 0;
+		this->mVects[i][1] = 0;
+		this->mVects[i][2] = 0;
+	}
+
+	print_if_false(pOwner != 0, "What? No pointer to owner?");
+	print_if_false(!pData || pData->mNumParams <= 2, "Too many params passed.");
+	print_if_false(!pData || pData->mNumVects <= 2, "Too many vects passed.");
+
+	CMessage *pOwnerMessage = pOwner->pMessage;
+	this->mNext = pOwnerMessage;
+
+	if (pOwnerMessage)
+		pOwnerMessage->pPrev = this;
+
+	pOwner->pMessage = this;
+
+	this->pBaddy = pOwner;
+	this->mHandle = Mem_MakeHandle(a2);
+
+	this->field_14 = a4;
+
+
+	if (pData)
+	{
+		for (i32 j = 0; j < pData->mNumParams; j++)
+		{
+			this->mParams[j] = pData->mParams[j];
+		}
+
+		for (i32 k = 0; k < pData->mNumVects; k++)
+		{
+			i32* pVec = pData->pVects[k];
+			this->mVects[k][0] = pVec[0];
+			this->mVects[k][1] = pVec[1];
+			this->mVects[k][2] = pVec[2];
+		}
+	}
 }
 
 // @Ok
