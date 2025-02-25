15d0fdb3effbaa72961031b2a901ad8cea413a2d
krystalgamer <callofdutypsp@gmail.com>
2025-02-25 18:08:34 +0100

    Reloc_Unload(u32)

--- reloc.cpp
+++ reloc.cpp
@@ -3,12 +3,43 @@
 
 #include "validate.h"
 
-// @BIGTODO
-void Reloc_Unload(u32)
+EXPORT SReloc *gRelocRoot;
+
+// @Ok
+// @Matching
+INLINE void Reloc_Unload(u32 crc)
 {
+	if (gRelocRoot)
+	{
+		SReloc *pSearch = gRelocRoot;
+
+		while (pSearch)
+		{
+			if (pSearch->mCRC == crc)
+			{
+				if (pSearch->field_0)
+					pSearch->field_0();
+
+				if (pSearch->pPrev)
+					pSearch->pPrev->pNext = pSearch->pNext;
+
+				if (pSearch->pNext)
+					pSearch->pNext->pPrev = pSearch->pPrev;
+
+				if (gRelocRoot == pSearch)
+					gRelocRoot = pSearch->pNext;
+
+				Mem_Delete(pSearch);
+				return;
+			}
+
+			pSearch = pSearch->pNext;
+		}
+	}
 }
 
 // @Ok
+// @Matching
 void Reloc_Unload(char* pStr)
 {
 	Reloc_Unload(Utils_GenerateCRC(pStr));
@@ -40,12 +71,13 @@ void Reloc_UnloadAll(void)
 
 void validate_SReloc(void)
 {
-	VALIDATE_SIZE(SReloc, 0x30);
+	VALIDATE_SIZE(SReloc, 0x34);
 
 	VALIDATE(SReloc, field_0, 0x0);
 	VALIDATE(SReloc, mCRC, 0x8);
 	VALIDATE(SReloc, field_C, 0xC);
 	VALIDATE(SReloc, pNext, 0x2C);
+	VALIDATE(SReloc, pPrev, 0x30);
 
 }
 
--- reloc.h
+++ reloc.h
@@ -5,14 +5,17 @@
 
 #include "export.h"
 
+typedef void (*relocFunc)(void);
+
 struct SReloc
 {
-	u32 field_0;
+	relocFunc field_0;
 	u8 padAfter0[4];
 
 	u32 mCRC;
 	u32 field_C[8];
 	SReloc *pNext;
+	SReloc *pPrev;
 };
 
 struct reloc_mod
@@ -20,7 +23,7 @@ struct reloc_mod
 };
 
 EXPORT void Reloc_Unload(char*);
-EXPORT void Reloc_Unload(unsigned int);
+EXPORT void Reloc_Unload(u32);
 EXPORT void Reloc_CallUserFunction(const char*, u32, const u32*, u32*);
 EXPORT void NullFunc(void);
 EXPORT void Reloc_Load(char *,i32);
