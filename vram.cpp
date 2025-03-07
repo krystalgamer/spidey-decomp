a94bf3e13c5507dfdbff74bb004bfe505a899a93
krystalgamer <callofdutypsp@gmail.com>
2025-03-07 19:05:16 +0100

    VRAMRectPack

--- vram.cpp
+++ vram.cpp
@@ -6,10 +6,36 @@
 
 EXPORT tagSVRAMRect *gVramRect;
 
-// @SMALLTODO
-void VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32)
+// @Ok
+// @Matching
+tagSVRAMRect* VRAMRectPack(
+		i32 Type,
+		i32 WW,
+		i32 H,
+		i32 *X,
+		i32 *Y,
+		i32 BPP,
+		u8 a7,
+		u32 a8)
 {
-    printf("VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32)");
+	tagSVRAMRect *v8 = static_cast<tagSVRAMRect *>(
+			DCMem_New(sizeof(tagSVRAMRect), 0, 1, 0, 1));
+
+	v8->p_next = gVramRect;
+	gVramRect = v8;
+
+	v8->p_poly = 0;
+	v8->update_type = 0;
+
+	v8->flags = a7;
+	v8->flags |= (BPP == 4 ? 4 : 0);
+	v8->flags |= (BPP == 8 ? 8 : 0);
+	v8->flags |= (BPP == 16 ? 16 : 0);
+	v8->flags |= (Type >= 0 ? 32 : 0);
+
+
+	v8->p_pack = Pack_Pack(Type, WW, H, X, Y, BPP, (a7 >> 1) & 1, a8);
+	return v8;
 }
 
 // @Ok
@@ -41,6 +67,10 @@ void validate_tagSVRAMRect(void)
 {
 	VALIDATE_SIZE(tagSVRAMRect, 0x14);
 
+	VALIDATE(tagSVRAMRect, flags, 0x0);
+
 	VALIDATE(tagSVRAMRect, p_pack, 0x04);
+	VALIDATE(tagSVRAMRect, update_type, 0xC);
+
 	VALIDATE(tagSVRAMRect, p_next, 0x10);
 }
--- vram.h
+++ vram.h
@@ -8,15 +8,19 @@
 
 struct tagSVRAMRect
 {
-	u8 padtop[4];
+	u8 flags;
+	u8 padAfter1[3];
 
 	SPack *p_pack;
-	u8 padAfter4[0x10-0x8];
+	void *p_poly;
+	u8 update_type;
+
+	u8 padAfter4[0x10-0xC-1];
 
 	tagSVRAMRect *p_next;
 };
 
-EXPORT void VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32);
+EXPORT tagSVRAMRect* VRAMRectPack(i32,i32,i32,i32 *,i32 *,i32,u8,u32);
 EXPORT void VRAMRectUnpack(tagSVRAMRect *);
 
 void validate_tagSVRAMRect(void);
