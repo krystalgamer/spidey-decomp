19699efc6367078eb2dfa2a2c59b5b16d79a75a8
krystalgamer <callofdutypsp@gmail.com>
2025-03-25 19:02:19 +0100

    Post_WaterEffect

--- post.cpp
+++ post.cpp
@@ -1,8 +1,12 @@
 #include "post.h"
 #include "bit.h"
+#include "PCGfx.h"
+#include "m3dinit.h"
+#include "SpideyDX.h"
 
 EXPORT i32 gWaterEffect;
 EXPORT i32 gPostTimerRelated;
+EXPORT i32 gPostWaterEffect;
 EXPORT u16 gPostWordRelated;
 
 // @SMALLTODO
@@ -18,7 +22,7 @@ void Post_DoPausePaletteProcessing(void)
 }
 
 // @Ok
-// @Validate: when WaterEffect inlined
+// @Matching
 void Post_PostProcessEffects(void)
 {
 	if (gWaterEffect)
@@ -44,8 +48,36 @@ void Post_UndoPausePaletteProcessing(void)
     printf("Post_UndoPausePaletteProcessing(void)");
 }
 
-// @SMALLTODO
-void Post_WaterEffect(void)
+// @Ok
+// @Matching
+// @Note: getting the v1 and v2 assignment was insane I had wrong code thank god I checked the size
+INLINE void Post_WaterEffect(void)
 {
-    printf("Post_WaterEffect(void)");
+	if (!gPostWaterEffect)
+	{
+		i32 v1 = rcossin_tbl[(gPostTimerRelated / 2) & 0xFFF].sin >> 6;
+		i32 v2 = rcossin_tbl[(gPostTimerRelated / 2) & 0xFFF].cos >> 6;
+
+		PCGfx_UseTexture(1, DCGfx_BlendingMode_1);
+		f32 v3 = gGameResolutionY;
+		f32 a3 = Yres;
+		f32 v8 = v3 / a3 * 240.0f;
+		f32 a3a = gGameResolutionX;
+
+		f32 v4 = Xres;
+		f32 a3b = a3a / v4 * 512.0f;
+
+		PCGfx_DrawQuad2D(
+			0.0,
+			0.0,
+			a3b,
+			v8,
+			0.0,
+			0.0,
+			1.0,
+			1.0,
+			(my_abs(v1) + 64) << 8 | (my_abs(v2) + 64) << 16 | 0x30000032,
+			6.0,
+			0);
+	}
 }
