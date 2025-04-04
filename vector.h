90c2c9789348d554abc4d4c6948cdf2b1b78d064
krystalgamer <callofdutypsp@gmail.com>
2025-04-04 19:14:56 +0200

    refactor: removed assignement operator from CVector

--- vector.h
+++ vector.h
@@ -12,61 +12,53 @@
 
 class CVector  
 {
-public: 
+	public: 
 
-	i32 vx;
-	i32 vy;
-	i32 vz;
+		i32 vx;
+		i32 vy;
+		i32 vz;
 
-	EXPORT INLINE CVector(i32 x, i32 y, i32 z)
-	{
-		this->vx = x;
-		this->vy = y;
-		this->vz = z;
-	}
+		EXPORT INLINE CVector(i32 x, i32 y, i32 z)
+		{
+			this->vx = x;
+			this->vy = y;
+			this->vz = z;
+		}
 
-	EXPORT INLINE CVector(void)
-	{
-		this->vx = 0;
-		this->vy = 0;
-		this->vz = 0;
-	}
+		EXPORT INLINE CVector(void)
+		{
+			this->vx = 0;
+			this->vy = 0;
+			this->vz = 0;
+		}
 
-	EXPORT INLINE void Set(i32 a1, i32 a2, i32 a3)
-	{
-		this->vx = a1;
-		this->vy = a2;
-		this->vz = a3;
-	}
+		EXPORT INLINE void Set(i32 a1, i32 a2, i32 a3)
+		{
+			this->vx = a1;
+			this->vy = a2;
+			this->vz = a3;
+		}
 
-	EXPORT INLINE void Set(CVector *a1)
-	{
-		this->Set(a1->vx, a1->vy, a1->vz);
-	}
+		EXPORT INLINE void Set(CVector *a1)
+		{
+			this->Set(a1->vx, a1->vy, a1->vz);
+		}
 
-	//EXPORT CVector(i32, i32, i32);
-	EXPORT void KillSmall();
-	EXPORT CVector* operator-=(const CVector& other);
-	EXPORT CVector* operator>>=(const int& other);
-	EXPORT CVector* operator<<=(const int& other);
-	EXPORT CVector* operator*=(const int& other);
-	EXPORT CVector* operator/=(const int& other);
-	EXPORT CVector* operator+=(const CVector& other);
-	EXPORT CVector* operator%=(const CFriction& other);
-	EXPORT i32 operator!=(const CVector& other);
-	EXPORT void Zero(void);
+		//EXPORT CVector(i32, i32, i32);
+		EXPORT void KillSmall();
+		EXPORT CVector* operator-=(const CVector& other);
+		EXPORT CVector* operator>>=(const int& other);
+		EXPORT CVector* operator<<=(const int& other);
+		EXPORT CVector* operator*=(const int& other);
+		EXPORT CVector* operator/=(const int& other);
+		EXPORT CVector* operator+=(const CVector& other);
+		EXPORT CVector* operator%=(const CFriction& other);
+		EXPORT i32 operator!=(const CVector& other);
+		EXPORT void Zero(void);
 
-	EXPORT i32 LengthSquared(void);
+		EXPORT i32 LengthSquared(void);
 
-	EXPORT const CVector& operator=(const CVector& rhs) {
-
-		this->vx = rhs.vx;
-		this->vy = rhs.vy;
-		this->vz = rhs.vz;
-
-		return *this;
-	}
-	EXPORT int Length(void);
+		EXPORT int Length(void);
 };
 
 EXPORT CVector operator/(const CVector& lhs, const int& other);
@@ -85,31 +77,31 @@ EXPORT CVector operator<<(const CVector& lhs, const i32& other);
 
 class CSVector{
 
-public:
-	i16 vx;
-	i16 vy;
-	i16 vz;
+	public:
+		i16 vx;
+		i16 vy;
+		i16 vz;
 
-	// @Ok
-	EXPORT INLINE CSVector(void)
-	{
-		this->vx = 0;
-		this->vy = 0;
-		this->vz = 0;
-	}
+		// @Ok
+		EXPORT INLINE CSVector(void)
+		{
+			this->vx = 0;
+			this->vy = 0;
+			this->vz = 0;
+		}
 
-	// @Ok
-	// Note: Guessed the name
-	EXPORT INLINE void Set(i16 a1, i16 a2, i16 a3)
-	{
-		this->vx = a1;
-		this->vy = a2;
-		this->vz = a3;
-	}
+		// @Ok
+		// Note: Guessed the name
+		EXPORT INLINE void Set(i16 a1, i16 a2, i16 a3)
+		{
+			this->vx = a1;
+			this->vy = a2;
+			this->vz = a3;
+		}
 
-	EXPORT CSVector* operator+=(const CSVector& other);
-	EXPORT void Mask(void);
-	EXPORT void KillSmall(void);
+		EXPORT CSVector* operator+=(const CSVector& other);
+		EXPORT void Mask(void);
+		EXPORT void KillSmall(void);
 };
 
 struct SVector {
