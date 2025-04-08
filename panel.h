efb6d4575bf36372368ac11bc042af55bd83b824
krystalgamer <callofdutypsp@gmail.com>
2025-04-08 17:34:14 +0200

    POLY_FT4

--- panel.h
+++ panel.h
@@ -11,6 +11,56 @@
 
 struct POLY_FT4
 {
+	// offset: 0000
+	u32 tag;
+	// offset: 0004
+	u8 r0;
+	// offset: 0005
+	u8 g0;
+	// offset: 0006
+	u8 b0;
+	// offset: 0007
+	u8 code;
+	// offset: 0008
+	i16 x0;
+	// offset: 000A
+	i16 y0;
+	// offset: 000C
+	u8 u0;
+	// offset: 000D
+	u8 v0;
+	// offset: 000E
+	u16 clut;
+	// offset: 0010
+	i16 x1;
+	// offset: 0012
+	i16 y1;
+	// offset: 0014
+	u8 u1;
+	// offset: 0015
+	u8 v1;
+	// offset: 0016
+	u16 tpage;
+	// offset: 0018
+	i16 x2;
+	// offset: 001A
+	i16 y2;
+	// offset: 001C
+	u8 u2;
+	// offset: 001D
+	u8 v2;
+	// offset: 001E
+	u16 pad1;
+	// offset: 0020
+	i16 x3;
+	// offset: 0022
+	i16 y3;
+	// offset: 0024
+	u8 u3;
+	// offset: 0025
+	u8 v3;
+	// offset: 0026
+	u16 pad2;
 };
 
 struct POLY_GT4
