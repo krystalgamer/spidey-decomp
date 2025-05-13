e31643f64c899e58b7ef7fb035ecc3342de2b0c8
krystalgamer <callofdutypsp@gmail.com>
2025-05-13 17:25:17 +0200

    build: old build scripts

---
+++ build.bat
@@ -0,0 +1,8 @@
+set MSVCDir=C:\vs
+
+set PATH=%MSDevDir%\BIN;%MSVCDir%\BIN;%PATH%
+
+set INCLUDE=%MSVCDir%\ATL\INCLUDE;%MSVCDir%\INCLUDE;%MSVCDir%\MFC\INCLUDE;%INCLUDE%
+set LIB=%MSVCDir%\LIB;%MSVCDir%\MFC\LIB;%LIB%
+
+nmake /f "spider.mak" CFG="spider - Win32 Release" SPIDEY_DIR="C:\spidey-decomp"
